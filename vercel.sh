2c879ce47de07e3f061bf3d59e3cbf8d197e2f6d
ibtihadj <ibtihadj.dev@gmail.com>
2023-02-10 23:32:14 +0000

    update vercel.sh

--- vercel.sh
+++ vercel.sh
@@ -1,2 +1,5 @@
+apt-get update
+sudo apt install mysql-client
+apt-get install -y libmariadb-dev-compat libmariadb-dev
 sudo apt-get install -y default-libmysqlclient-dev
 pip3 install --disable-pip-version-check --target . --upgrade -r requirements.txt
