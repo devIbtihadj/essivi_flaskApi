12b10b9784b139275886279fcb941ac0ec47d9af
ibtihadj <ibtihadj.dev@gmail.com>
2023-02-11 01:03:06 +0000

    add vercel.py

---
+++ vercel.py
@@ -0,0 +1,7 @@
+import subprocess
+
+subprocess.run("sudo apt-get update")
+subprocess.run("pip install libmysqlclient-dev")
+subprocess.run("sudo apt-get install -y libmysqlclient-dev")
+subprocess.run("pip3 install mysqlclient")
+subprocess.run("sudo apt-get install -y python3-dev")
