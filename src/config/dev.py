5553799b658bf3a733149c3f42c023288b934978
ibtihadj <ibtihadj.dev@gmail.com>
2023-02-12 08:14:32 +0000

    change database url

--- src/config/dev.py
+++ src/config/dev.py
@@ -5,17 +5,17 @@ load_dotenv()
 
 DEBUG = True
 SECRET_KEY = '@2023!secret?iai_project'
-# SQLALCHEMY_DATABASE_URI = ("mysql+pymysql://" + os.getenv('DB_USER') + ":"
-#                            + os.getenv('DB_PASSWORD') + "@"
-#                            + os.getenv('DB_HOST')
-#                            + ":3306/essivi_dbv2")
+SQLALCHEMY_DATABASE_URI = ("mysql+pymysql://" + os.getenv('DB_USER') + ":"
+                           + os.getenv('DB_PASSWORD') + "@"
+                           + os.getenv('DB_HOST')
+                           + ":3306/essivi_dbv2")
 
 
-SQLALCHEMY_DATABASE_URI = ("mysql+pymysql://" + "sql8597335" + ":"
-                           + "jDsJXggtIR" + "@"
-                           + "sql8.freesqldatabase.com"
-                           + ":3306/sql8597335")
-
+# SQLALCHEMY_DATABASE_URI = ("mysql+pymysql://" + "sql8597335" + ":"
+#                            + "jDsJXggtIR" + "@"
+#                            + "sql8.freesqldatabase.com"
+#                            + ":3306/sql8597335")
+#
 
 
 # connect-string: mysql+mysqldb://<user>:<password>@<host>[:<port>]/<dbname>
